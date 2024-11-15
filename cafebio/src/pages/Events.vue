4a50cb712130725e58d53bc372e71d6f5fc35f27
alch29 <christensen.alek@gmail.com>
2024-11-15 12:04:59 +0100

    rettelse af router

--- cafebio/src/pages/Events.vue
+++ cafebio/src/pages/Events.vue
@@ -1,9 +1,31 @@
 <script setup>
+    import { ref, onMounted, watchEffect } from 'vue';
+    import { fetchEventsFromFirebase } from '../firebaseService';  // Importér funktionen fra firebaseService
 
+    const events = ref([]);  // Opret en reaktiv liste til at gemme events
+
+    async function loadEvents() {
+      events.value = await fetchEventsFromFirebase();  // Hent events fra Firebase og gem i `events`
+    }
+
+    onMounted(loadEvents);  // Kald loadEvents, når komponenten monteres
+    
+     // Overvåg ændringer og genindlæs events
+    watchEffect(async () => {
+      events.value = await fetchEventsFromFirebase();
+    });
 </script>
 
 <template>
+    <div class="events-container">
+        <div>
+            <h1>Arrangementer</h1>
+        </div>
+        <div class="event" v-for="event in events" :key="event.id">
+          <router-link :to="`/events/${event.id}`">{{ event.name }}</router-link>
 
+        </div>
+    </div>
 </template>
 
 <style scoped>
