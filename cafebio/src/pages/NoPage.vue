e2c08b7ec7cccf8af38c2a01838e1fd1881d2db9
alch29 <christensen.alek@gmail.com>
2024-11-19 16:20:15 +0100

    tilføjelse af under konstruktion-side

---
+++ cafebio/src/pages/NoPage.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+</script>
+
+<template> 
+    <div class="container">
+        <div class="text">
+            <h1>Hov! Denne side eksisterer ikke</h1>
+            <p>Da dette kun er en prototype, er flere sider stadig under konstruktion.</p>
+        </div>
+        <div>
+            <img src="../assets/images/Sommerfugl rød.png" alt="Sommerfugl logo i rød farve.">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .container {
+        height: 100vh;
+        width: auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 38px;
+    }
+
+    img {
+        max-height: 180px;
+    }
+
+    .text {
+        max-width: 240px;
+    }
+
+    h1 {
+        margin-bottom: 8px;
+    }
+
+    @media (max-width: 680px) {
+        .container {
+            flex-direction: column-reverse;
+        }
+
+        img {
+            margin-bottom: 18px;
+        }
+    }
+</style>
--- cafebio/src/router/index.js
+++ cafebio/src/router/index.js
@@ -3,12 +3,14 @@ import Home from '../pages/Home.vue';
 import Events from '../pages/Events.vue';
 import EventDetail from '../pages/EventDetail.vue';
 import Contact from '../pages/Contact.vue';
+import NoPage from '../pages/NoPage.vue';
 
 const routes = [
   { path: '/', component: Home },           //"startside"
   { path: '/events', name: 'Events', component: Events },
   { path: '/events/:id', name: 'EventDetail', component: EventDetail },  // Dynamisk rute til hver event
-  { path: '/contact', name: 'Contact', component: Contact }
+  { path: '/contact', name: 'Contact', component: Contact },
+  { path: '/no-page', name: 'NoPage', component: NoPage }
 ];
 
 const router = createRouter({
