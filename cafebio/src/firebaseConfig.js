4e2d8b052902313d57a81213170c837cc491489b
freyajoy <freyajoyhaycock@gmail.com>
2024-11-29 10:33:00 +0100

    Update firebaseConfig.js

--- cafebio/src/firebaseConfig.js
+++ cafebio/src/firebaseConfig.js
@@ -1,19 +1,22 @@
-// src/firebaseConfig.js
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCOx32YweLEvO7KZVvrDKnKp6dK96mBDl4",
-    authDomain: "update-event-8f0f6.firebaseapp.com",
-    databaseURL: "https://update-event-8f0f6-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "update-event-8f0f6",
-    storageBucket: "update-event-8f0f6.appspot.com",
-    messagingSenderId: "1402621778",
-    appId: "1:1402621778:web:cc160ff6d40e0b4da0348b"
-  };
+  apiKey: "AIzaSyDHMsaxnaPkJ4qM4jc6_AlksoCqf8y-xNg",
+  authDomain: "eksamen-tihi-vi-doer.firebaseapp.com",
+  databaseURL: "https://eksamen-tihi-vi-doer-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "eksamen-tihi-vi-doer",
+  storageBucket: "eksamen-tihi-vi-doer.firebasestorage.app",
+  messagingSenderId: "110120621437",
+  appId: "1:110120621437:web:5ef8b91101b4245dbde814",
+  measurementId: "G-ET09YTWLDL"
+};
 
-// Initialiser Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-export { database };
+const analytics = getAnalytics(app);
