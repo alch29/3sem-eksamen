7d12571d40c0a0507f2573b62fdc2d49f0609811
freyajoy <freyajoyhaycock@gmail.com>
2024-11-29 10:48:22 +0100

    ...

--- cafebio/src/firebaseConfig.js
+++ cafebio/src/firebaseConfig.js
@@ -1,11 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyDHMsaxnaPkJ4qM4jc6_AlksoCqf8y-xNg",
   authDomain: "eksamen-tihi-vi-doer.firebaseapp.com",
