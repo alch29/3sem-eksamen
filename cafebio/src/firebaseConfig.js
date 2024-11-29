c1535c3715f3b21e7809cc51dc482e3b4052ba9b
freyajoy <freyajoyhaycock@gmail.com>
2024-11-29 11:00:03 +0100

    .

--- cafebio/src/firebaseConfig.js
+++ cafebio/src/firebaseConfig.js
@@ -1,15 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDHMsaxnaPkJ4qM4jc6_AlksoCqf8y-xNg",
-  authDomain: "eksamen-tihi-vi-doer.firebaseapp.com",
-  databaseURL: "https://eksamen-tihi-vi-doer-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "eksamen-tihi-vi-doer",
-  storageBucket: "eksamen-tihi-vi-doer.firebasestorage.app",
-  messagingSenderId: "110120621437",
-  appId: "1:110120621437:web:5ef8b91101b4245dbde814",
-  measurementId: "G-ET09YTWLDL"
+  apiKey: "AIzaSyCOx32YweLEvO7KZVvrDKnKp6dK96mBDl4",
+  authDomain: "update-event-8f0f6.firebaseapp.com",
+  databaseURL: "https://update-event-8f0f6-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "update-event-8f0f6",
+  storageBucket: "update-event-8f0f6.firebasestorage.app",
+  messagingSenderId: "1402621778",
+  appId: "1:1402621778:web:cc160ff6d40e0b4da0348b"
 };
 
 // Initialize Firebase
